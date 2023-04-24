e34e2e27a75be18faad18451e703a56a6c643826
itzB0N <130971019+itzB0N@users.noreply.github.com>
2023-04-24 11:50:41 +0300

    Update MainWindow.xaml.cs

--- dop_2.2/MainWindow.xaml.cs
+++ dop_2.2/MainWindow.xaml.cs
@@ -26,15 +26,61 @@ namespace dop_2._2
         }
         private void OK_Click_1(object sender, RoutedEventArgs e)
         {
-            int n = Convert.ToInt32(polA.Text);
+              int n = Convert.ToInt32(polA.Text);
             double nak = 0;
             for (int i = 1; i <= n; i++)
             {
                 nak += 1 / Math.Pow(i, 8);
-                if (Math.Abs(nak) < Math.E)
+                if (Math.Abs(nak) < n)
                 {
-                    Otv.Content = $"{nak}";
+                    Otv.Content = $"a) {nak}\n";
                 }
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                nak += 1 / (i*(i+1));
+                if (Math.Abs(nak) < n)
+                {
+                    Otv1.Content = $"б) {nak}";
+                }
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                nak += Math.Pow(-1,i) /(i * i +1);
+                if (Math.Abs(nak) < n)
+                {
+                    Otv2.Content = $"в) {nak}";
+                }
+            }
+
+            for (int i = 0; i <= n; i++)
+            {
+                nak += Math.Pow(-2, i) / (i * i + 1);
+                if (Math.Abs(nak) < n)
+                {
+                    Otv3.Content = $"г) {nak}";
+                }
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                nak += Math.Pow(-1, i + 1) / (i * (i + 1) * (i + 2));
+                if (Math.Abs(nak) < n)
+                {
+                    Otv4.Content = $"д) {nak}";
+                }
+            }
+
+            for (int i = 0; i <= n; i++)
+            {
+                nak += 1 / (Math.Pow(4,i) + Math.Pow(5, i+2));
+                if (Math.Abs(nak) < n)
+                {
+                    Otv5.Content = $"e) {nak}";
+                }
+            }
 
             }
         }
